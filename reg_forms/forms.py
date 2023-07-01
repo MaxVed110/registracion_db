589f7d384cd33d5209b5386d73d259d4e0c2faf4
Maxim <vedernikovmaxim@rambler.ru>
2023-07-01 23:03:13 +0300

    small changes

--- reg_forms/forms.py
+++ reg_forms/forms.py
@@ -12,11 +12,6 @@ class RegForm(FlaskForm):
     submit = SubmitField(label='Submit')
 
     def validate_email(self, email):
-        # count the number of user ids for that email
-        # if it's not 0, there's a user with that email already
-        print(1234)
         a = db.session.query(db.func.count(User.id)).filter_by(email=email.data).scalar()
-        print(a)
-        print(email.data)
         if a:
             raise ValidationError('The email is already registered in the system')
